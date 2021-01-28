413757f0bc4ddf728fba5506de343d48f8ebe6e5
gozdetoz <gozdetoz62@gmail.com>
2021-01-28 21:28:50 +0300

    Add project files.

---
+++ MemberRegistration.ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemberRegistration.Business.Abstract;
+using MemberRegistration.Business.Concrete;
+using MemberRegistration.Business.DependencyResolvers.Ninject;
+using MemberRegistration.Entities.Concrete;
+
+namespace MemberRegistration.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var memberService = InstanceFactory.GetInstance<IMemberService>();
+            memberService.Add(new Member{Email = "engindemirog@gmail.com", FirstName = "Engin", LastName = "Demiroğ", DateOfBirth = new DateTime(1985,1,6), TcNo = "28861499108"});
+            Console.WriteLine("Eklendi");
+            Console.ReadLine();
+        }
+    }
+}
